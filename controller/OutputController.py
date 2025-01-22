91dbe6f2ff4ec7988d774aaf3873ab9955bcfc43
Norbert Balaz <n.balaz@outlook.com>
2025-01-22 18:24:45 +0100

    refactor: seperates outputs and services

--- controller/OutputController.py
+++ controller/OutputController.py
@@ -15,6 +15,7 @@ class OutputController():
         self.debug = debug
         self._logger = self._intialize_logger()
         self.services = None
+        self.outputs = None
         self.queue_threads = []
 
     def _intialize_logger(self):
@@ -24,44 +25,49 @@ class OutputController():
     
     def start(self):
         self._setup()
-        self._start_services()
+        self._start_services_and_outputs()
         self._start_queue_threads()
         self._start_gui()
         self._logger.info("All services started")
 
     def stop(self):
         self._stop_queue_threads()
-        self._stop_services()
+        self._stop_services_and_outputs()
 
     def _start_gui(self):
-        self.services[ServicesEnum.ImageDisplayOutput].trigger_action()
+        self.outputs[ServicesEnum.ImageDisplayOutput].trigger_action()
 
     def _setup(self):
-        self.services = {
-            # ServicesEnum.FaceRecognition: FaceRecognition(service_name = ServicesEnum.FaceRecognition.value, debug = False),
-            ServicesEnum.UltrasonicSensor: UltrasonicSensor(service_name = ServicesEnum.UltrasonicSensor.value, debug = False),
-            # ServicesEnum.TouchSensor: TouchSensor(service_name = ServicesEnum.TouchSensor.value, debug = False),
+        self.outputs = {
             ServicesEnum.ImageDisplayOutput: ImageDisplayOutput(service_name = ServicesEnum.ImageDisplayOutput.value, debug = False)
         }
+        self.services = {
+            # ServicesEnum.FaceRecognition: FaceRecognition(service_name = ServicesEnum.FaceRecognition.value, debug = False),
+            ServicesEnum.UltrasonicSensor: UltrasonicSensor(service_name = ServicesEnum.UltrasonicSensor.value, debug = True),
+            # ServicesEnum.TouchSensor: TouchSensor(service_name = ServicesEnum.TouchSensor.value, debug = False),
+        }
         
-    def _start_services(self):
+    def _start_services_and_outputs(self):
         """
         Initialize and start all services.
         """
         for service in self.services.values():
-            service.start()#
-    
+            service.start()
+        
+        for output in self.outputs.values():
+            output.start()
+
     def _start_queue_threads(self):
         for service in self.services.values():
             queue_thread = QueueListenerThread(
                 service=service,
-                target_output=self.services[ServicesEnum.ImageDisplayOutput].incoming_queue,
-                debug=self.debug
+                target_output=self.outputs[ServicesEnum.ImageDisplayOutput].incoming_queue,
+                debug=service.debug
             )
             self.queue_threads.append(queue_thread)
             queue_thread.start()
 
-    def _stop_services(self):
+    def _stop_services_and_outputs(self):
         """
         Stop and clean up all services.
         """
@@ -70,6 +76,12 @@ class OutputController():
                 service.stop()
             except KeyboardInterrupt:
                 self._logger.warning(f"Interrupted while stopping {service.service_name}")
+
+        for output in self.outputs.values():
+            try:
+                output.stop()
+            except KeyboardInterrupt:
+                self._logger.warning(f"Interrupted while stopping {output.service_name}")
     
     def _stop_queue_threads(self):
         for thread in self.queue_threads:
