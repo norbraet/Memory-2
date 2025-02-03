2c752f0178182fde6b1a1dd351bb39e00daf4d00
Norbert Balaz <n.balaz@outlook.com>
2025-02-03 19:35:38 +0100

    fix: fixes base config

--- dataclass/BaseConfig.py
+++ dataclass/BaseConfig.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 class BaseConfig:
     level_steps: int = 0.5
     level_steps_max: int = 2
-    level_steps_min: int = 0.1
-    level_steps_interval = 0.1
+    level_steps_min: float = 0.1
+    level_steps_interval: float = 0.1
     restoration_duration: int = 2
     restoration_duration_max: int = 5
-    restoration_duration_min: int = 0.1
-    restoration_duration_interval: int = 0.1
+    restoration_duration_min: float = 0.1
+    restoration_duration_interval: float = 0.1
